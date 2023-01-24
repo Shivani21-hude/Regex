907f94c25b4946a5648a48ac454a376acc26a6b0
Shivani21-hude <shivani21hude@gmail.com>
2023-01-24 15:54:24 +0530

    [Shivani]-Check User First Name valid or not

---
+++ Regex_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Regex_Problem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  ****  User Registration Process  ****\n");
+            UserRegistration.ValidFirstName();
+        }
+    }
+}
---
+++ Regex_Problem/UserRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Problem
+{
+    internal class UserRegistration
+    {
+        public static void ValidFirstName()
+        {
+            Regex FirstName = new Regex("^[A-Z]{1}[a-z]{2,}$");
+            Console.Write("Enter your first Name :");
+            string firstname = Console.ReadLine();
+            if (FirstName.IsMatch(firstname))
+            {
+                Console.WriteLine("-> Name  : " + firstname);
+            }
+            else
+            {
+                Console.WriteLine("Your entered name is in invalid pattern");
+            }
+        }
+    }
+}
